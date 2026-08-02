6d0fa207a6c0b701f5601330f0d443dc57c2ce7d
winfreyguez <rodriguezgeorge2025@gmail.com>
2026-08-03 00:03:08 +0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,15 @@
-const CACHE_NAME = 'family-hub-v1';
+const CACHE_NAME = 'family-hub-v2';
 const urlsToCache = ['/', '/index.html'];
 
 self.addEventListener('install', (event) => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', (event) => {
