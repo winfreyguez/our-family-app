37cff5897df7a319846db75da5553f66717a07f7
winfreyguez <rodriguezgeorge2025@gmail.com>
2026-08-02 12:14:36 +0300

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Hardcoded keys - this will FORCE the app to work!
 const supabaseUrl = 'https://vnxtrumkvuvsuhhvucjm.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZueHRydW1rdnV2c3VoaHZ1Y2ptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU2MzI1MzgsImV4cCI6MjEwMTIwODUzOH0.MtstxOdT4TAE_hyx5hAhV8dSWDah-ni87KqYvIHLH90'
 
